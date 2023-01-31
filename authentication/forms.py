e4f2ab4ca3b81c338bf5caefe9ff3eac4240f12f
osiris03 <lorierrinel@gmail.com>
2023-01-31 17:24:05 +0100

    initialize connection page

---
+++ authentication/forms.py
@@ -0,0 +1,8 @@
+# authentication/forms.py 
+# longinform
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=63, label='Nom d`utilisateur')
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# import des fonction login et authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from . import forms 
 
-# Create your views here.
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ' '
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request,user)
+                massege = f'Bonjour, {user.username}! Vous êtes connecté.'
+            else:
+                message = 'identifiants invalides'
+    return render(request,'authentication/login.html',context={'form':form,'message':message})
