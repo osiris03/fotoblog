df99055d5d6e6340c443ef447bdf9bcbb7f98405
osiris03 <lorierrinel@gmail.com>
2023-02-02 11:02:02 +0100

    i am add password change and password change done

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 import authentication.views
 import blog.views 
@@ -30,6 +32,15 @@ urlpatterns = [
         template_name ='authentication/login.html',
         redirect_authenticated_user = True
     ), name='login'),
+
+    path('',LoginView.as_view(
+    template_name ='authentication/login.html',
+    redirect_authenticated_user = True
+    ), name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
+    
+    path('change-password/', PasswordChangeView.as_view(template_name='authentication/password_change_form.html'),name='password_change'),
+    path('change-password-done', PasswordChangeDoneView.as_view(template_name='authentication/password_change_done.html'),name = 'password_change_done'),
+
     path('home/', blog.views.home, name='home'),
 ]
