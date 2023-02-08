cc565f344141f4e5fca0c159865a5d323685aced
osiris03 <lorierrinel@gmail.com>
2023-02-08 16:37:27 +0100

    create photo and blog model in blog folder

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,8 @@
 # authentication/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+
 
 class User(AbstractUser):
     
@@ -13,3 +15,4 @@ class User(AbstractUser):
     )
     profile_photo = models.ImageField(verbose_name='Photo de profil')
     role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings 
 
 # Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField()
+    caption = models.CharField(max_length=128, blank=True)
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add = True )
+
+class Blog(models.Model):
+    photo = models.ForeignKey(Photo, null=True, blank = True, on_delete = models.SET_NULL)
+    title = models.CharField(max_length=128)
+    content = models.CharField(max_length=5000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete = models.CASCADE, blank= True)
+    date_created = models.BooleanField(default = False)
+
